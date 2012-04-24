6679b242753b7bfe3eaddaf65dd465cf2b261fbc
Brian Zhang <tangerinewhite32@gmail.com>
2012-04-24 02:16:26 -0400

    - added decay function for integration error in StasisController.cs - added missing CurrentError variable assignment in PID.cs - move variable declarations to head of file in StasisController.cs
    Signed-off-by: Brian Zhang <tangerinewhite32@gmail.com>

--- software/netduino/Stasis/StasisController.cs
+++ software/netduino/Stasis/StasisController.cs
@@ -8,9 +8,14 @@ namespace Stasis.Software.Netduino
 {
 	public class StasisController
 	{
-		const int DISPLAY_COUNT = 50;
-		const int MAX_MOTOR_OUTPUT = 150;   // max motor value is actually 100, but sin(output) rarely reaches 1.0 -BZ (4/22/12)
-		const int SATURATION = 30;
+		const int displayCount = 50;
+		const int maxMotorOutput = 150;   // max motor value is actually 100, but sin(output) rarely reaches 1.0 -BZ (4/22/12)
+		const int saturation = 30;
+        const int stableRange = 1;
+        int counter = displayCount;
+        int motorValue = 0;
+        double nonLinearOuput = 0.0;
+        DateTime lastDateTime = DateTime.Now;
 
 		/// <summary>
 		/// Gets the balbot being controlled
@@ -24,8 +29,10 @@ namespace Stasis.Software.Netduino
 		/// <summary>
 		/// PID for motor speed control
 		/// </summary>
-		private PID pidLogic = new PID(proportionalConstant: 2.25);      // Current value for nonlinear output // Linear Controller: P ~ 5.75 -BZ (4/22/12)
-		private MedianFilter medianFilter = new MedianFilter(3);
+		private PID pidLogic = new PID(proportionalConstant: 2.2, integrationConstant: 0.005, derivativeConstant: 1);      // Current value for nonlinear output // Linear Controller: P ~ 5.75 -BZ (4/22/12)
+		private MedianFilter IRFilter = new MedianFilter(3);
+        private MedianFilter accelFilter = new MedianFilter(3);
+        private Sensors.Analog5DOF imu = new Sensors.Analog5DOF(Pins.GPIO_PIN_A0, Pins.GPIO_PIN_A1, Pins.GPIO_PIN_A2, Pins.GPIO_PIN_A3);
 
 		/// <summary>
 		/// Constructor
@@ -34,7 +41,7 @@ namespace Stasis.Software.Netduino
 		public StasisController(StasisRobot bot)
 		{
 			this.Robot = bot;
-			this.pidLogic.SetPoint = 90.5;        // slightly tilted; should calibrate at the beginning of each run -BZ (4/12/12)
+			this.pidLogic.SetPoint = 91.75;        // slightly tilted; should calibrate at the beginning of each run -BZ (4/12/12)
 		}
 
 		/// <summary>
@@ -61,12 +68,6 @@ namespace Stasis.Software.Netduino
 			this.pidLogic.SetPoint = 91;
 		}
 
-		int counter = DISPLAY_COUNT;
-		int motorValue = 0;
-		double nonLinearOuput = 0.0;
-		DateTime lastDateTime = DateTime.Now;
-		Sensors.Analog5DOF imu = new Sensors.Analog5DOF(Pins.GPIO_PIN_A0, Pins.GPIO_PIN_A1, Pins.GPIO_PIN_A2, Pins.GPIO_PIN_A3);
-
 		/// <summary>
 		/// Performs one iteration of the controller
 		/// </summary>
@@ -76,43 +77,56 @@ namespace Stasis.Software.Netduino
 
 			// Let the robot update it's state
 			this.Robot.Update();
+            this.imu.Update();
 
-			// Filter tile values
-			this.medianFilter.AddValue(this.Robot.Tilt);
-
+			// Filter tilt values
+            this.IRFilter.AddValue(this.Robot.Tilt);      // takes 5 fps
+            //this.accelFilter.AddValue(this.imu.Acceleration.Z);
+            
 			// Update the PID 
-			this.pidLogic.Update(medianFilter.Value);
+            this.pidLogic.Update(IRFilter.Value);
+            //this.pidLogic.Update(this.Robot.Tilt);  // unfiltered value
 
 			// Apply Nonlinear Map
-			nonLinearOuput = MAX_MOTOR_OUTPUT * XMath.Sin(this.pidLogic.Output / 180 * System.Math.PI);
+            nonLinearOuput = maxMotorOutput * XMath.Sin(this.pidLogic.Output / 180 * System.Math.PI);     // takes 5 fps
 
 			// Setup motors for new PID output
-			if (this.medianFilter.Value > (this.pidLogic.SetPoint - SATURATION) && this.medianFilter.Value < (this.pidLogic.SetPoint + SATURATION))
+            //if (this.Robot.Tilt > (this.pidLogic.SetPoint - saturation) && this.Robot.Tilt < (this.pidLogic.SetPoint + saturation))
+            if (this.IRFilter.Value > (this.pidLogic.SetPoint - saturation) && this.IRFilter.Value < (this.pidLogic.SetPoint + saturation))
 			{
-				motorValue = (int)System.Math.Round(nonLinearOuput); //(int)System.Math.Round(this.pidLogic.Output);
+                motorValue = (int)System.Math.Round(nonLinearOuput); //(int)System.Math.Round(this.pidLogic.Output);
+
+                // Decaying function for integration error
+                if (this.IRFilter.Value > (this.pidLogic.SetPoint - stableRange) && this.IRFilter.Value < (this.pidLogic.SetPoint + stableRange))
+                {
+                    this.pidLogic.accumulativeError /= 2;
+                }
 			}
 
 			this.Robot.LeftMotor.Speed = this.Robot.RightMotor.Speed = motorValue;
-			imu.Update();
 
-			// Display debug output every DISPLAY_COUNT cycles
-			if (counter > 0)
-			{
-				counter--;
-			}
-			else
-			{
-				// Measuring the time delta of each cycle
-				var diff = (double)(DateTime.Now - lastDateTime).Ticks;
-				var fps = (DISPLAY_COUNT / diff) * TimeSpan.TicksPerSecond;
+            // Toggle Debug display
+            if (false) 
+            {
+                // Display debug output every displayCount cycles
+                if (counter > 0)
+                {
+                    counter--;
+                }
+                else
+                {
+                    // Measuring the time delta of each cycle
+                    var diff = (double)(DateTime.Now - lastDateTime).Ticks;
+                    var fps = (displayCount / diff) * TimeSpan.TicksPerSecond;
 
-				//Debug.Print(fps.ToString());
-				Debug.Print(fps + ">>" + motorValue + "\t:\t" + this.medianFilter.Value + "\t:\t" + imu.Acceleration.ToString());
+                    //Debug.Print(fps.ToString());
+                    Debug.Print(motorValue + "\t:\t" + this.Robot.Tilt + "\t:\t" + this.pidLogic.accumulativeError + "\t:\t" + this.pidLogic.DerivativeError);
+                    //Debug.Print(fps + ">>\t" + motorValue + "\t:\t" + this.Robot.Tilt + "\t:\t" + imu.Acceleration.ToString() + "\t:\t" + imu.RotationRate.ToString());
 
-				lastDateTime = DateTime.Now;
-				counter = DISPLAY_COUNT;
-			}
+                    lastDateTime = DateTime.Now;
+                    counter = displayCount;
+                }
+            }
 		}
-
 	}
 }
