4733c7fb797f6e825abc8504be93173df05b791f
Aditya Gaddam <adityagaddam@gmail.com>
2012-04-24 19:28:24 -0400

    "WiFi command center almost done. Signed-off-by: Aditya Gaddam <adityagaddam@gmail.com>"

---
+++ software/pc/command_center/StasisCommandCenter/StasisCommandCenter/WiFiMonitorConnection.Message.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+
+namespace StasisCommandCenter
+{
+	public partial class WiFiMonitorConnection
+	{
+		public class Message
+		{
+			/// <summary>
+			/// Gets the type of message received from the PC
+			/// </summary>
+			public string Type
+			{
+				get;
+				private set;
+			}
+
+			/// <summary>
+			/// Gets the actual values from the message
+			/// </summary>
+			public double[] Values
+			{
+				get;
+				private set;
+			}
+
+			/// <summary>
+			/// Constructor #1
+			/// </summary>
+			/// <param name="type"></param>
+			/// <param name="data"></param>
+			public Message(string type, double[] values)
+			{
+				this.Type = type;
+				this.Values = values;
+			}
+
+			/// <summary>
+			/// Constructor #2
+			/// </summary>
+			/// <param name="rawMessage"></param>
+			/// <param name="rawMessageLength"></param>
+			public Message(byte[] rawMessage, int rawMessageLength)
+			{
+				byte[] rawMessageRealSize = new byte[rawMessageLength];
+				for (int i = 0; i < rawMessageLength; i++)
+				{
+					rawMessageRealSize[i] = rawMessage[i];
+				}
+				string s = new String(Encoding.UTF8.GetChars(rawMessageRealSize));
+				var parts = s.Split(new char[] { ',' });
+
+				this.Type = parts[0];
+				this.Values = new double[parts.Length - 2];
+				// Start at index 1 and go to count - 1 (last index is \r\n)
+				for (int i = 1; i < parts.Length - 1; i++)
+				{
+					this.Values[i - 1] = double.Parse(parts[i]);
+				}
+			}
+		}
+
+		/// <summary>
+		/// Types of messages
+		/// </summary>
+		public class MessageType
+		{
+			public const string SetPID = "SPID";
+			public const string GetPID = "GPID";
+			public const string GetLoopSpeed = "GLS";
+		}
+
+		/// <summary>
+		/// Delegate for MessageReceived
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		public delegate void MessageReceivedEventHandler(object sender, MessageReceivedEventArgs e);
+
+		/// <summary>
+		/// Event data for MessageRecevied
+		/// </summary>
+		public class MessageReceivedEventArgs
+		{
+			/// <summary>
+			/// Gets the message that was received
+			/// </summary>
+			public Message Message
+			{
+				get;
+				private set;
+			}
+
+			/// <summary>
+			/// Constructor
+			/// </summary>
+			/// <param name="message"></param>
+			public MessageReceivedEventArgs(Message message)
+			{
+				this.Message = message;
+			}
+
+		}
+	}
+}
---
+++ software/pc/command_center/StasisCommandCenter/StasisCommandCenter/WiFiMonitorConnection.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Sockets;
+using System.ComponentModel;
+using System.IO;
+using System.Diagnostics;
+
+namespace StasisCommandCenter
+{
+	public partial class WiFiMonitorConnection
+	{
+		/// <summary>
+		/// 
+		/// </summary>
+		public event EventHandler Connected = delegate { };
+		
+		/// <summary>
+		/// 
+		/// </summary>
+		public event EventHandler Disconnected = delegate { };
+
+		/// <summary>
+		/// Message receieved from the robot
+		/// </summary>
+		public event MessageReceivedEventHandler MessageReceived = delegate { };
+
+		/// <summary>
+		/// Connection to module
+		/// </summary>
+		private TcpClient connection = new TcpClient();
+
+		/// <summary>
+		/// Background read worker for the connection
+		/// </summary>
+		private BackgroundWorker worker;
+
+		/// <summary>
+		/// Constructor
+		/// </summary>
+		public WiFiMonitorConnection()
+		{
+
+		}
+
+		/// <summary>
+		/// Connect to the robot
+		/// </summary>
+		/// <param name="ip"></param>
+		/// <param name="port"></param>
+		public void Connect(string ip, int port)
+		{
+			this.connection.Connect(ip, port);
+			this.Connected(this, EventArgs.Empty);
+
+			this.worker = new BackgroundWorker();
+			this.worker.DoWork += new DoWorkEventHandler(Worker_DoWork);
+			this.worker.WorkerSupportsCancellation = true;
+			this.worker.RunWorkerAsync();
+		}
+
+		/// <summary>
+		/// Disconnect from the robot
+		/// </summary>
+		public void Disconnect()
+		{
+			this.worker.CancelAsync();
+			this.connection.Close();
+			this.Disconnected(this, EventArgs.Empty);
+		}
+
+		/// <summary>
+		/// Sends a message back to robot
+		/// </summary>
+		/// <param name="message"></param>
+		public void SendMessage(Message message)
+		{
+			string output = message.Type + ",";
+			foreach (var val in message.Values)
+			{
+				output += val.ToString("N3") + ",";
+			}
+			output += "\r\n";
+			byte[] outBytes = Encoding.UTF8.GetBytes(output);
+			this.connection.GetStream().Write(outBytes, 0, outBytes.Length);
+			this.connection.GetStream().Flush();
+		}
+
+		private void Worker_DoWork(object sender, DoWorkEventArgs e)
+		{
+			byte[] rawMessage = new byte[1024];
+			int rawMessageLength = 0;
+			while (this.connection.Connected && this.worker.CancellationPending == false)
+			{
+				if (this.connection.Available > 0)
+				{
+					byte b = (byte)this.connection.GetStream().ReadByte();
+
+					rawMessage[rawMessageLength++] = b;
+					if (b == 0x0A && rawMessage[rawMessageLength - 2] == 0x0D)
+					{
+						// Got a full message
+						var m = new Message(rawMessage, rawMessageLength);
+
+						// Let people know
+						this.MessageReceived(this, new MessageReceivedEventArgs(m));
+
+						// Reset message
+						rawMessageLength = 0;
+					}
+				}
+			}
+		}
+	}
+}
