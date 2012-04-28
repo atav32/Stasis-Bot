4313500dc9763e7cd98c8a3768563aa15bb2fdc4
Aditya Gaddam <adityagaddam@gmail.com>
2012-04-27 20:55:51 -0400

    "Removing binaries from PIC project Signed-off-by: Aditya Gaddam <adityagaddam@gmail.com>"

--- software/pic/encoder_divider/Encoder_Divider.X/build/default/debug/_ext/1472/main.o.d
+++
@@ -1,8 +0,0 @@
-build/default/debug/_ext/1472/main.o: ../main.c \
- ../lib/base/include/tsilib.h ../lib/base/include/generic_types.h \
- ../lib/base/include/tsilib.h ../lib/base/include/pinio.h \
- ../lib/base/include/impl/pic32/pinio_pic32.h \
- ../lib/base/include/impl/pic32/pin_constants_pic32.h \
- ../lib/base/include/uartlib.h ../lib/base/include/intadclib.h \
- ../lib/base/include/i2clib.h ../lib/base/include/fifo.h \
- ../lib/base/include/netio.h
